01c9dae442e8235a13c6e5f0bc3212cc176ce419
Santana Jolly <santanaharris@gmail.com>
2019-09-19 10:20:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List
+            List<int> list = new List<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                list.Add(i);
+            }
+
+            //LinkedList
+            LinkedList<int> linkList = new LinkedList<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                linkList.AddLast(i);
+            }
+
+            //Queue
+            Queue<int> queue = new Queue<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                queue.Enqueue(i);
+            }
+        }
+
+    }
+}
+
+
